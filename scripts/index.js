41217d766182aee1b370e70a5d7db7602c959a95
Francisco García <francoffee57@gmail.com>
2022-02-01 18:38:19 -0600

    Funcionalidad del DarkMode hecha

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 const body = document.getElementById('body')
 
-alert('fsdaf')
+const buttonDark = document.getElementById('dark')
+const buttonWhite = document.getElementById('white')
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    body.classList.add('dark')
+}
+
+buttonWhite.addEventListener('click',()=>{
+    body.classList.remove('dark')
+})
+
+buttonDark.addEventListener('click',()=>{
+    body.classList.add('dark')
+})
